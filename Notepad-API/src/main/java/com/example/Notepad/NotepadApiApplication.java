a606eef136d25ebd6e54a38f84f38aa0ce922dfd
shakib522 <shakibhasann522@gmail.com>
2024-02-09 12:25:27 +0600

    get all note by type implemented

--- Notepad-API/src/main/java/com/example/Notepad/NotepadApiApplication.java
+++ Notepad-API/src/main/java/com/example/Notepad/NotepadApiApplication.java
@@ -2,9 +2,17 @@ package com.example.Notepad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NotepadApiApplication {
+public class NotepadApiApplication extends SpringBootServletInitializer {
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotepadApiApplication.class, args);
